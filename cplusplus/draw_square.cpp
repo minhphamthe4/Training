1c5c41c67ffed48f1c51fdffd091b61f234491c2
minhphamthe4 <minhphamthe4@gmail.com>
2026-03-12 23:37:49 +0700

    Add C++ program to draw a square

--- cplusplus/draw_square.cpp
+++ cplusplus/draw_square.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"+ - - - - + - - - - +"<<endl;
+    cout<<"|         |         |"<<endl;
+    cout<<"|         |         |"<<endl;
+    cout<<"|         |         |"<<endl;
+    cout<<"|         |         |"<<endl;
+    cout<<"+ - - - - + - - - - +"<<endl;
+    cout<<"|         |         |"<<endl;
+    cout<<"|         |         |"<<endl;
+    cout<<"|         |         |"<<endl;
+    cout<<"|         |         |"<<endl;
+    cout<<"+ - - - - + - - - - +"<<endl;
+}
 
